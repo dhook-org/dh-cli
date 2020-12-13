dac750690ab753f9656d39a6de0be4ba71665d7f
Ichii <45918948+IchiiDev@users.noreply.github.com>
2020-12-13 20:42:28 +0100

    Exported app instance for further imports

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ argv.splice(0, 2);
 import { app } from "src/app";
 // Require the main CLI App (This file is just the command execution file)
 
-app.run(argv);
+let cli = app.run(argv);
+
+export { app };
