367012ca8d5b53ae3e7605c460d4a077cbdd55d9
Ichii <45918948+IchiiDev@users.noreply.github.com>
2020-12-13 20:18:14 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+require("./src/app");
+// Require the main CLI App (This file is just the command execution file)
---
+++ src/app.js
@@ -0,0 +1,4 @@
+const argv = process.argv;
+argv.splice(0, 2);
+
+module.exports = { argv };
