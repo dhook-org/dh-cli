9030d51e6f66ced86113e93f931f7c13ab38f5c7
Ichii <45918948+IchiiDev@users.noreply.github.com>
2020-12-14 09:09:18 +0100

    Completed default help command

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import kleur from "kleur";
 import { jsonRequire } from "./functions/jsonRequire.js";
+import fs from "fs";
 
 export class app {
 
@@ -7,17 +8,20 @@ export class app {
         this.argv = null;
     }
 
-    run(argv) {
+    async run(argv) {
         this.argv = argv;
         
         if (argv[0] == null) {
-            const commands_config = jsonRequire("configs/commands.json");
-            const commands_list = commands_config.commands;
-            const commands_categories = commands_config.categories;
+            const commands_config = await jsonRequire("configs/commands.json");
+            const categories = commands_config.categories;
 
             let commands = "";
-            for (let category in commands_categories) {
-                commands += "/t" + category + ":"
+            for (let category in categories) {
+                commands += "\t" + categories[category].name + ":\n";
+                for (let command in categories[category].commands) {
+                    command = categories[category].commands[command];
+                    commands += "\t- " + command.name + "\t| " + command.description;
+                }
             }
 
             console.log(`${kleur.red("[ERROR]")} please specify an argument.\n\n${commands}`);
