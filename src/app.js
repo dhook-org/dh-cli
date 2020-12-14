abf2865aa3a6b9983afbeabba255709655c74699
Ichii <45918948+IchiiDev@users.noreply.github.com>
2020-12-14 11:06:26 +0100

    Fix indentation error on console output

--- src/app.js
+++ src/app.js
@@ -10,20 +10,21 @@ export class app {
     async run(argv) {
         this.argv = argv;
         
+        // default command response, will show commands list if no args are passed.
         if (argv[0] == null) {
             const commands_config = await jsonRequire("configs/commands.json");
             const categories = commands_config.categories;
 
             let commands = "";
             for (let category in categories) {
-                commands += "\t" + categories[category].name + ":\n";
+                commands += "\n\t" + categories[category].name + ":\n";
                 for (let command in categories[category].commands) {
                     command = categories[category].commands[command];
-                    commands += "\t- " + command.name + "\t| " + command.description;
+                    commands += "\t- " + command.name + ((command.name.length < 6) ? "\t": "") + "\t| " + command.description + "\n";
                 }
             }
 
-            console.log(`${kleur.red("[ERROR]")} please specify an argument.\n\n${commands}`);
+            console.log(`${kleur.red("[ERROR]")} please specify an argument.\n${commands}`);
         }
     }
 
