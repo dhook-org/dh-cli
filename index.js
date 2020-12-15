b7aee61b45cce13d5154e23e3757b820a8c07c32
Ichii <45918948+IchiiDev@users.noreply.github.com>
2020-12-15 15:50:47 +0100

    Added missing semicolon

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ argv.splice(0, 2);
 import { app } from "./src/app.js";
 // Require the main CLI App (This file is just the command execution file)
 
-export let cli = new app(argv)
+export let cli = new app(argv);
