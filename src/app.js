c1875d415d6aec7c72a9402baf25ce951846f3a7
Ichii <45918948+IchiiDev@users.noreply.github.com>
2020-12-15 15:06:10 +0100

    Add global installation check

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 // Functions import
 import { help } from "./functions/helpdisplay.js";
+import { getInstalledPath } from "get-installed-path";
 
 export class app {
 
     constructor() {
         this.argv = null;
+        this.current = null;
     }
 
     async run(argv) {
         this.argv = argv;
-        
+        this.current = await getInstalledPath("dh-cli").catch(err => {
+            throw "Can't find dh-cli path. Is it installed globally ?";
+        });
+
         switch (argv[0]) {
             // Help command, will display every single commands of dh-cli.
             case "help":
