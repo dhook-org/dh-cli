2386ee300a45cd02f97aff67f8cb80d6f9d998a6
Ichii <45918948+IchiiDev@users.noreply.github.com>
2020-12-20 16:43:46 +0100

    Fix config functions

--- src/utils/configs.js
+++ src/utils/configs.js
@@ -1,19 +1,23 @@
-import fs from "fs/promises";
-import cli from "../../index.js";
+import fsp from "fs/promises";
+import fs from "fs";
+import { cli } from "../../index.js";
 import yaml from "yaml";
+import { resolve } from "path";
+import { rejects } from "assert";
 
-export async function get(file) {
-
-    await fs.readFile(`${cli.config_path}\\${file}`, (err, data) => {
-        if (err) throw err;
-        return yaml.parse(data);
+export async function getConfig(file) {
+    return new Promise((resolve, rejects) => {
+        fs.readFile(`${cli.config_path}\\${file}`, "utf8", (err, data) => {
+            if (err) throw err;
+            resolve(yaml.parse(data));
+        });
     });
 
 }
 
-export async function write(file, data) {
+export async function writeConfig(file, data) {
 
-    await fs.writeFile(`${cli.config_path}\\${file}`, yaml.stringify(data), "utf-8", err => {
+    await fsp.writeFile(`${cli.config_path}\\${file}`, yaml.stringify(data), "utf-8", err => {
         if (err) throw err;
     });
 
