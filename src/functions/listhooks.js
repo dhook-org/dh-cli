dd19981caaa47180eb23ed7ef7fd63bda89018c6
Ichii <45918948+IchiiDev@users.noreply.github.com>
2020-12-21 22:27:21 +0100

    Fix deepscan issues (3ea9a66 analysis)

--- src/functions/listhooks.js
+++ src/functions/listhooks.js
@@ -1,11 +1,9 @@
 import kleur from "kleur";
-import { parseSeq } from "yaml/util";
 import { getConfig } from "../utils/configs.js";
 
 export async function listHooks() {
     const profile_config = await getConfig("hooks");
     console.log(kleur.blue("Hooks list:"));
-    let column = 0
     let list = "";
     for (let hook in profile_config.hooks) {
         if (profile_config.hooks[hook] == null) continue;
